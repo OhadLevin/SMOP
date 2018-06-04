b9ec49a1152795108726f3a4b26d9f5936036ee5
amirisr <amir.39.talpiot@gmail.com>
2018-06-04 13:51:25 +0300

    /

--- AudioUtil.py
+++ AudioUtil.py
@@ -9,8 +9,10 @@ FILE_OUT = "output\\out-{}.wav"
 
 
 def slicer(startmili, endmili, file_inputdir):
-    file_outdir = FILE_OUT.format(file_inputdir.replace(".wav","").replace(
-        "\\", "_") + "_from_" + str(startmili) + "_to_" + str(endmili))
+    file_outdir = FILE_OUT.format(file_inputdir.replace(".wav",
+                                                        "").replace("\\",
+                                                                    "_") +
+                                  "slice-from-" + str(startmili) + "-to-" + str(endmili))
     inp = wave.open(file_inputdir, 'rb')
     fr = inp.getframerate()
     start = math.floor(startmili * fr / 1000)
@@ -28,8 +30,29 @@ def slicer(startmili, endmili, file_inputdir):
     out.close()
     return file_outdir
 
-
+def concat(*files):
+    file_outdir = FILE_OUT.format("HI".replace(".wav","").replace("\\","_") + "concat")
+    data = b''
+    sw = 0
+    fr = 0
+    c = 0
+    for i in range(len(files)):
+        tmp = wave.open(files[i], 'rb')
+        data += tmp.readframes(tmp.getnframes())
+        c = tmp.getnchannels()
+        fr = tmp.getframerate()
+        sw = tmp.getsampwidth()
+        tmp.close()
+    out = wave.open(file_outdir, 'wb')
+    out.setnchannels(c)
+    out.setframerate(fr)
+    out.setsampwidth(sw)
+    out.writeframes(data)
+    out.close()
+    return file_outdir
 
 if __name__ == '__main__':
-    slicer(1000, 2000, FILE)
+    #slicer(1000, 2000, FILE)
+    f = slicer(1000, 2000, FILE)
+    concat(f,f,f)
 
